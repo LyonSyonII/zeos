9e70039b1d3b94c32464332fdf2c5f1e751c1902
lyonsyonii <liam@garriga.dev>
2024-09-15 18:22:51 +0200

    added shell flake.nix

--- flake.nix
+++ flake.nix
@@ -3,21 +3,27 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, pkgs, ... }@inputs: flake-utils.lib.eachDefaultSystem (system: {
-    devShells.default = pkgs.mkShell rec {
-      nativeBuildInputs = with pkgs; [
-        gdb
-        llvmpackages.bintools
-      ];
+  outputs = { self, nixpkgs, flake-utils, pkgs, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          gdb
+          llvmpackages.bintools
+        ];
 
-      buildInputs = with pkgs; [
-        stdenv
-        gnumake
-        ccls
-        pkg-config
-      ];
+        buildInputs = with pkgs; [
+          stdenv
+          gnumake
+          ccls
+          pkg-config
+        ];
 
-      LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath nativeBuildInputs;
-    };
-  });
+        LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath nativeBuildInputs;
+      };
+    });
 }
