c732bd450925830f13a906c3d3df74983baa91ae
lyonsyonii <liam@garriga.dev>
2024-10-04 12:27:37 +0200

    add setup scripts

---
+++ liamsetup.sh
@@ -0,0 +1,4 @@
+./so2setup.sh
+
+git config --global user.email liam@garriga.dev
+git config --global user.name lyonsyonii
---
+++ urielsetup.sh
@@ -0,0 +1,4 @@
+./so2setup.sh
+
+git config --global user.email urikogi@gmail.com
+git config --global user.name uriel
