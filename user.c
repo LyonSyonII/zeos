c85d0b0de40b1a7ee2631e09efc5510f5e2d5428
daf <Urikoji>
2024-10-06 18:48:12 +0200

    afegit una prova per veure com s'imprimeix un text molt llarg i una altra per veure si funciona perror()

--- user.c
+++ user.c
@@ -39,9 +39,14 @@ int __attribute__ ((__section__(".text.main")))
   // char* p = 0;
   // *p = 'x';
   // int result = addASM(99, 85);
+  //printlntest("test de com de llarg podem fer el missatge sense que exploti i tal perque bueno i si volem escriure un missatge molt llarg i tal que? i si ho he entes be hem d'utilitzar la funcio aquesta de copy_from_user i copy_to_user encara que funcioni sense aixo i he ficat un parche molt cutre perque no tenim malloc per fer-ho be");
 
   printlntest("Hello ZeOS from user!");
   printlntest("\nIf you read this message, I'm alive");
+
+  //crida que falla (fd incorrecte)
+  write(0, "alo", 3);
+  perror();
 
   while(1) { }
 }
