7e93b603618dad7e6dd890f45857c7955e2b0afb
lyonsyonii <liam@garriga.dev>
2024-10-10 17:27:56 +0200

    chore: clean up user.c

--- user.c
+++ user.c
@@ -15,29 +15,28 @@ void printlntest(const char* buffer) {
 int __attribute__ ((__section__(".text.main")))
   main(void)
 {
-    /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
-     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
-  // char* p = 0;
-  // *p = 'x';
-  // int result = addASM(99, 85);
-  //printlntest("test de com de llarg podem fer el missatge sense que exploti i tal perque bueno i si volem escriure un missatge molt llarg i tal que? i si ho he entes be hem d'utilitzar la funcio aquesta de copy_from_user i copy_to_user encara que funcioni sense aixo i he ficat un parche molt cutre perque no tenim malloc per fer-ho be");
+  // Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception
+  // __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) );
+
+
   
   /// WRITE ///
   int written = 0;
 
   printlntest("\nHello ZeOS from user!");
   printlntest("\nIf you read this message, I'm alive");
+  // printlntest("text molt llarg per veure que copy_from_user funciona: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum, nunc vitae convallis dignissim, eros est faucibus tellus, non bibendum nisl lacus quis eros. Mauris eu felis vehicula, ornare ante quis, tincidunt tortor. Nam velit sem, convallis id posuere quis, finibus non purus. Vivamus eget mollis lectus. Proin ullamcorper consectetur risus, vitae mollis augue efficitur at. Praesent molestie nec turpis vitae lobortis. Pellentesque vehicula lacus quis ipsum blandit pulvinar. Aenean est risus, faucibus in feugiat sed, laoreet a eros. Integer congue metus in vestibulum eleifend. Ut eget egestas nunc, quis vestibulum urna. Suspendisse in risus vel nibh rhoncus aliquet. Praesent hendrerit est sit amet arcu porttitor posuere. Nullam libero turpis, semper at lectus nec, volutpat finibus magna. Duis elementum mauris ex, ac aliquet urna interdum convallis. Nullam massa nulla, pretium efficitur congue vitae, pulvinar quis dui. Fusce ultrices laoreet augue ut ultrices. Cras eu diam placerat, finibus lectus ac, ultrices purus. Cras elementum leo vitae venenatis accumsan. Curabitur accumsan pretium sagittis. Vivamus consectetur lobortis nulla, ut lacinia nunc congue at. Aliquam diam felis, congue ut condimentum eu, venenatis sed enim. Fusce sit amet erat ac neque porta hendrerit pulvinar at nulla. Maecenas gravida tincidunt magna ut auctor. Proin ac efficitur.");
+  
+  // Uncomment to test PAGE FAULT
+  // char* p = 0; *p = 'x';
 
   // Crida que falla (fd incorrecte)
   written = write(0, "alo", 3); 
   if (written < 0) perror();
 
-  //Test per null pointer
+  // Test per null pointer
   written = write(1, (char*)0, 3);
   if (written < 0) perror();
-
-  volatile int __aaa = *(volatile int*)(0);
-  __aaa = 5;
 
   /// GETTIME ///
   while(1) {
