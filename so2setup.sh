c5dcd51fd7cb3f5dbc9c1f075376126e03726f13
lyonsyonii <liam@garriga.dev>
2024-10-11 12:53:45 +0200

    fix github login

--- so2setup.sh
+++ so2setup.sh
@@ -4,3 +4,4 @@ code --install-extension llvm-vs-code-extensions.vscode-clangd
 code --install-extension 13xforever.language-x86-64-assembly
 
 gh auth login -w
+gh config set -h github.com git_protocol https
