5e38f24ead1e2f162e8c1d1545757c72d8db99b4
lyonsyonii <liam@garriga.dev>
2024-11-22 13:07:57 +0100

    add printf family

--- zeos/include/io.h
+++ zeos/include/io.h
@@ -6,6 +6,7 @@
 #define __IO_H__
 
 #include <types.h>
+#include <sched.h>
 
 /** Screen functions **/
 /**********************/
@@ -14,5 +15,27 @@ Byte inb (unsigned short port);
 void printc(char c);
 void printc_xy(Byte x, Byte y, char c);
 void printk(char *string);
+
+
+void printkint(int i);
+void printkintln(int i);
+void printkhex(int i);
+void printkhexln(int i);
+void printkptr(const void* ptr);
+void printkptrln(const void* ptr);
+
+// Supports: `%d`, `%p`, `%x`, `%s`.
+#define printkf(template, ...) __printkf(template, (const void*[]) { __VA_ARGS__ })
+void __printkf(const char* template, const void* args[]);
+void dbg_task(struct task_struct* task);
+
+void __printkf(const char* template, const void* args[]);
+void __dummy(const char* template, const void* args[]);
+
+#if defined(DEBUG) && DEBUG > 0
+    #define dbg(...) printkf(__VA_ARGS__)
+#else
+    #define dbg(template, ...) __dummy(template, (const void*[]) { __VA_ARGS__ })
+#endif
 
 #endif  /* __IO_H__ */
