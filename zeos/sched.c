41e58f92e1d59b793104a485a7c78b836c421764
lyonsyonii <liam@garriga.dev>
2024-12-09 20:52:26 +0100

    init allocated pages list

--- zeos/sched.c
+++ zeos/sched.c
@@ -212,6 +212,7 @@ void init_task1(void)
   set_user_pages(c);
   c->stack_start_page = PAG_LOG_INIT_DATA;
   c->stack_num_pages = NUM_PAG_DATA;
+  INIT_LIST_HEAD(&c->allocated_pages_list);
 
   tss.esp0=(DWord)&(uc->stack[KERNEL_STACK_SIZE]);
   setMSR(0x175, 0, (unsigned long)&(uc->stack[KERNEL_STACK_SIZE]));
